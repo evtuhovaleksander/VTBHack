93967f8a9316545e8d204fd7263724895b1d99a4
Evtuhov <Evtuhov@binbank.ru>
2019-09-14 11:52:21 +0300

    init

---
+++ VTBHack/VTBHack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VTBHack
+//
+//  Created by Aleksander Evtuhov on 14/09/2019.
+//  Copyright © 2019 RxProject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
