835620832c666cc675866a9c400919de69426a92
Evtuhov <Evtuhov@binbank.ru>
2019-09-14 14:00:34 +0300

    pods

---
+++ VTBHack/VTBHack/BuisinessLogic/Helper/Types.swift
@@ -0,0 +1,12 @@
+//
+//  Types.swift
+//  VTBHack
+//
+//  Created by Aleksander Evtuhov on 14/09/2019.
+//  Copyright © 2019 RxProject. All rights reserved.
+//
+
+import Foundation
+
+typealias VoidBlock = ()->()
+typealias ErrorBlock = (Error)->()
---
+++ VTBHack/VTBHack/BuisinessLogic/ServiceLayer/ServiceLayer.swift
@@ -0,0 +1,18 @@
+//
+//  ServiceLayer.swift
+//  VTBHack
+//
+//  Created by Aleksander Evtuhov on 14/09/2019.
+//  Copyright © 2019 RxProject. All rights reserved.
+//
+
+import Foundation
+
+class ServiceLayer {
+    static var shared = ServiceLayer()
+    
+    private init() {
+        
+    }
+    
+}
